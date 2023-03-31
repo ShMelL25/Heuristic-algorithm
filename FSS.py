54b1b3244bc5250f50ecdd6f1147529fefa907b4
Egor <egor2014ag@mail.ru>
2023-03-31 21:40:20 +0300

    Add fish_step and debug fix

--- FSS.py
+++ FSS.py
@@ -9,6 +9,7 @@ class FSS(object):
         self.fish
         self.fish_init
         self.weight
+        self.step
         self.step_fish
     
     
@@ -18,12 +19,11 @@ class FSS(object):
         
         weight_fish = self.weight(x = x_fish, y = y_fish, z = z_fish, i_j = i_j, weightScale = weightScale)
         
-        x_fish_new, y_fish_new, z_fish_new, i_j_new = self.step_fish(x, y, z, i = i_j[0], j = i_j[1])
+        x_fish_new, y_fish_new, z_fish_new, i_j_new = self.step(x, y, z, i = i_j[0], j = i_j[1])
         
         return i_j, i_j_new
         
         
-    # initialization fish
     def fish_init(self, x, y, z, populationSize):
         x_1 = []
         y_1 = []
@@ -38,8 +38,8 @@ class FSS(object):
             j = random.randint(0, len(x[i])-1)
 
             x_1.append(x[i][j])
-            y_1.append(y[i][j])
-            z_1.append(z[i][j])
+            y_1.append(x[i][j])
+            z_1.append(x[i][j])
             
             i_mass.append(i)
             j_mass.append(j)
@@ -48,15 +48,14 @@ class FSS(object):
         
         return x_1, y_1, z_1, i_j
         
-    #fish weight
+    
     def weight(self,x, y, z, i_j, weightScale):
         
         weight_fish=weightScale/2
         
         return weight_fish
     
-    #step fish
-    def step_fish(self, x, y, z, i, j):
+    def step(self, x, y, z, i, j):
         
         x_new_coord = []
         y_new_coord = []
@@ -70,67 +69,91 @@ class FSS(object):
             if ((i[step] != 99) and (j[step] != 99)) and ((i[step] != 0) and (j[step] != 0)):
                 a = random.randint(-1,1)
                 b = random.randint(-1,1)
-
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
             elif ((i[step] == 99) and (j[step] == 99)) and ((i[step] != 0) and (j[step] != 0)):
                     
                 a = random.randint(-1,0)
                 b = random.randint(-1,0)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                 
             elif (i[step] == 99) and (j[step] == 0):
                     
                 a = random.randint(-1,0)
                 b = random.randint(0,1)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
             elif (i[step] == 0) and (j[step] == 99):
                     
                 a = random.randint(0,1)
                 b = random.randint(-1,0)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
             elif (i[step] == 99) and (j[step] != 0):
                     
                 a = random.randint(-1,0)
                 b = random.randint(-1,1)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
             elif (j[step] == 99) and (i[step] != 0):
                     
                 b = random.randint(-1,0)
                 a = random.randint(-1,1)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                 
                     
             elif (i[step] == 0) and (j[step] == 0):
@@ -138,54 +161,58 @@ class FSS(object):
                 a = random.randint(0,1)
                 b = random.randint(0,1)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
             elif (i[step] == 0) and (j[step] != 99):
                         
                 a = random.randint(0,1)
                 b = random.randint(-1,1)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
             elif (j[step] == 0) and (i[step] != 99):
                     
                 a = random.randint(-1,1)
                 b = random.randint(0,1)
                     
-                x_new_coord.append(x[i[step]+a][j[step]+b])
-                y_new_coord.append(y[i[step]+a][j[step]+b])
-                z_new_coord.append(z[i[step]+a][j[step]+b])
-                i_new.append(i[step]+a)
-                j_new.append(j[step]+b)
+                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = x[i[step]][j[step]], z = z[i[step]][j[step]], 
+                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
+                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
+                
+                x_new_coord.append(x_step)
+                y_new_coord.append(y_step)
+                z_new_coord.append(z_step)
+                i_new.append(i_step)
+                j_new.append(j_step)
                     
         
         i_j_new = np.array([i_new, j_new])
         return x_new_coord, y_new_coord, z_new_coord, i_j_new
-        
-
-def f(x1,x2):
-    a=np.sqrt(np.fabs(x2+x1/2+47))
-    b=np.sqrt(np.fabs(x1-(x2+47)))
-    c=-(x2+47)*np.sin(a)-x1*np.sin(b)
     
-    return c
-
+    def step_fish(self, x, y, z, i, j,  x_new, y_new, z_new, i_new, j_new):
+        
+        if z > z_new:
+            return x_new, y_new, z_new, i_new, j_new
+        
+        else:
+            return x, y, z, i, j
+            
+        
+    
 FSS_alg = FSS()
-
-x1=np.linspace(-512,512,100)
-x2=np.linspace(-512,512,100)
-X1,X2=np.meshgrid(x1,x2)
-
-c = f(X1,X2)
-i, j = FSS_alg.fish(x=X1, y=X2, z=c)
-
-print(i)
-print(j)
