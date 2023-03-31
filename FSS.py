5d2f697e1df8ca465578386f894a89c7b22a35b7
Egor <egor2014ag@mail.ru>
2023-03-31 12:15:42 +0300

    gitignore

---
+++ FSS.py
@@ -0,0 +1,168 @@
+class FSS(object):
+    
+    def __init__(self):
+        
+        self.fish
+        self.fish_init
+        self.weight
+        self.step_fish
+    
+    
+    def fish(self, x, y, z, populationSize=100, iterationCount=10, lowerBoundPoint=None, higherBoundPoint=None, individStepStart=None, individStepFinal=None, weightScale=1000.0):
+        
+        x_fish, y_fish, z_fish, i_j = self.fish_init(x = x, y = y, z = z, populationSize=100)
+        
+        weight_fish = self.weight(x = x_fish, y = y_fish, z = z_fish, i_j = i_j, weightScale = weightScale)
+        
+        x_fish_new, y_fish_new, z_fish_new, i_j_new = self.step_fish(x, y, z, i = i_j[0], j = i_j[1])
+        
+        return i_j, i_j_new
+        
+        
+    def fish_init(self, x, y, z, populationSize):
+        x_1 = []
+        y_1 = []
+        z_1 = []
+        
+        i_mass = []
+        j_mass = []
+
+        for a in range(1, populationSize):
+
+            i = random.randint(0, len(x)-1)
+            j = random.randint(0, len(x[i])-1)
+
+            x_1.append(x[i][j])
+            y_1.append(x[i][j])
+            z_1.append(x[i][j])
+            
+            i_mass.append(i)
+            j_mass.append(j)
+        
+        i_j = np.array([i_mass, j_mass])
+        
+        return x_1, y_1, z_1, i_j
+        
+    
+    def weight(self,x, y, z, i_j, weightScale):
+        
+        weight_fish=weightScale/2
+        
+        return weight_fish
+    
+    def step_fish(self, x, y, z, i, j):
+        
+        x_new_coord = []
+        y_new_coord = []
+        z_new_coord = []
+        i_new = []
+        j_new = []
+        
+        
+        for step in range(0,len(i)):
+                
+            if ((i[step] != 99) and (j[step] != 99)) and ((i[step] != 0) and (j[step] != 0)):
+                a = random.randint(-1,1)
+                b = random.randint(-1,1)
+
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+            elif ((i[step] == 99) and (j[step] == 99)) and ((i[step] != 0) and (j[step] != 0)):
+                    
+                a = random.randint(-1,0)
+                b = random.randint(-1,0)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                
+            elif (i[step] == 99) and (j[step] == 0):
+                    
+                a = random.randint(-1,0)
+                b = random.randint(0,1)
+                    
+                x_new_coord.append(x[i[step]+a][step_j+b])
+                y_new_coord.append(y[i[step]+a][step_j+b])
+                z_new_coord.append(z[i[step]+a][step_j+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+            elif (i[step] == 0) and (j[step] == 99):
+                    
+                a = random.randint(0,1)
+                b = random.randint(-1,0)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+            elif (i[step] == 99) and (j[step] != 0):
+                    
+                a = random.randint(-1,0)
+                b = random.randint(-1,1)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+            elif (j[step] == 99) and (i[step] != 0):
+                    
+                b = random.randint(-1,0)
+                a = random.randint(-1,1)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                
+                    
+            elif (i[step] == 0) and (j[step] == 0):
+                    
+                a = random.randint(0,1)
+                b = random.randint(0,1)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+            elif (i[step] == 0) and (j[step] != 99):
+                        
+                a = random.randint(0,1)
+                b = random.randint(-1,1)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+            elif (j[step] == 0) and (i[step] != 99):
+                    
+                a = random.randint(-1,1)
+                b = random.randint(0,1)
+                    
+                x_new_coord.append(x[i[step]+a][j[step]+b])
+                y_new_coord.append(y[i[step]+a][j[step]+b])
+                z_new_coord.append(z[i[step]+a][j[step]+b])
+                i_new.append(i[step]+a)
+                j_new.append(j[step]+b)
+                    
+        
+        i_j_new = np.array([i_new, j_new])
+        return x_new_coord, y_new_coord, z_new_coord, i_j_new
+        
+    
+FSS_alg = FSS()
