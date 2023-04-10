a9790ede3fb09d5580770f461f6e5ce83be83290
Egor <egor2014ag@mail.ru>
2023-04-10 16:16:55 +0300

    Bug_fix and add test_file

--- FSS.py
+++ FSS.py
@@ -1,28 +1,59 @@
 import numpy as np
-import pandas as pd
 import random
+import math
+from numpy. linalg import norm
+from tqdm import tqdm 
 
 class FSS(object):
     
     def __init__(self):
-        
         self.fish
-        self.fish_init
+        self.fish_init_
+        self.fish_step
+        self.delta_f
         self.weight
-        self.step
-        self.step_fish
-    
-    
-    def fish(self, x, y, z, populationSize=100, iterationCount=10, lowerBoundPoint=None, higherBoundPoint=None, individStepStart=None, individStepFinal=None, weightScale=1000.0):
+        self.instinctive_collective_step
+        self.coll_step
         
-        coord_fish_iter, i_j = self.fish_init(x = x, y = y, z = z, populationSize=100)
+    def fish(self, x, y, z, populationSize=200, iterationCount=100, individStep = 1, weightScale = 500):
         
-        fish_new_coord, i_j_new, weight_new = self.step(x = x, y = y, z = z, i = i_j[0], j = i_j[1], weightScale = weightScale)
+        coord_mass = []
         
-        return coord_fish_iter, fish_new_coord, weight_new
+        coord_fish_start, i_j_coord = self.fish_init_(coord = [x,y,z], populationSize = populationSize) # inizialisation
         
+        coord_fish_one = coord_fish_start
         
-    def fish_init(self, x, y, z, populationSize):
+        coord_mass.append(coord_fish_start)
+
+        for i in tqdm(range(0,iterationCount)):
+            
+            coord_fish, i_j_coord = self.fish_step(coord = [x, y, z], i_j = i_j_coord, individStep = individStep)
+            
+            #coord_mass.append(coord_fish)
+
+            weightScale, delta_mass = self.weight(z_coord = [coord_fish_one[2], coord_fish[2]], weightScale = weightScale)
+            
+            coord_fish_one, i_j_coord = self.instinctive_collective_step(coord = [x, y, z], i_j = i_j_coord, z_coord = [coord_fish_one[2], coord_fish[2]])
+            
+            #coord_mass.append(coord_fish_one)
+            
+            weightScale, delta_mass = self.weight(z_coord = [coord_fish_one[2], coord_fish[2]], weightScale = weightScale)
+            
+            bary_c = self.barycenter(z_coord = coord_fish_one, weight = weightScale)
+            
+            coord_fish = coord_fish_one
+            
+            coord_fish_one, i_j_coord = self.coll_step(coord = [x, y, z], i_j = i_j_coord, z_coord = coord_fish_one, individStep = individStep, bary_center = bary_c)
+            
+            coord_mass.append(coord_fish_one)
+            
+        
+        coord_mass = np.array(coord_mass)
+            
+        return coord_mass
+        
+    def fish_init_(self, coord, populationSize):
+        x, y, z = coord[0], coord[1], coord[2]
         x_1 = []
         y_1 = []
         z_1 = []
@@ -30,7 +61,7 @@ class FSS(object):
         i_mass = []
         j_mass = []
 
-        for a in range(1, populationSize):
+        for a in range(0, populationSize):
 
             i = random.randint(0, len(x)-1)
             j = random.randint(0, len(x[i])-1)
@@ -46,72 +77,235 @@ class FSS(object):
         i_j_coord = np.array([i_mass, j_mass])
         
         return fish_coord, i_j_coord
-        
     
-    def weight(self, z_mass, weightScale):
-        
-        weight_fish = weightScale/2
-        wieght_iter_fish = []
-        
-        for i in range(0, len(z_mass[0])):
-            
-            wieght_iter_fish.append((z_mass[0][i] - z_mass[1][i]/ max(z_mass[0])) + weight_fish)
-            
-        wieght_iter_fish = np.array(wieght_iter_fish)    
-        
-        return wieght_iter_fish
     
-    def step(self, x, y, z, i, j, weightScale):
+    def fish_step(self, coord, i_j, individStep):
         
-        x_new_coord = []
-        y_new_coord = []
-        z_new_coord = []
+        a = random.randint(-1,1)
+        b = random.randint(-1,1)
+        
+        x_new = []
+        y_new = []
+        z_new = []
         i_new = []
         j_new = []
-        weigth_fish_iter = []
         
+        x, y, z = coord[0], coord[1], coord[2]
+        i, j = i_j[0], i_j[1]
         
         for step in range(0,len(i)):
-                
-            if ((i[step] != 99) and (j[step] != 99)) and ((i[step] != 0) and (j[step] != 0)):
-                a = random.randint(-1,1)
-                b = random.randint(-1,1)
-                
-                x_step, y_step, z_step, i_step, j_step = self.step_fish(x = x[i[step]][j[step]], y = y[i[step]][j[step]], z = z[i[step]][j[step]], 
-                                                   i = i[step], j = j[step], x_new = x[i[step]+a][j[step]+b], 
-                                                   y_new = y[i[step]+a][j[step]+b], z_new = z[i[step]+a][j[step]+b], i_new = i[step]+a, j_new = j[step]+b)
-                
-                x_new_coord.append(x_step)
-                y_new_coord.append(y_step)
-                z_new_coord.append(z_step)
-                i_new.append(i_step)
-                j_new.append(j_step)
-                    
             
-            else:
+            if (i[step]+a < len(z) and j[step]+b < len(z) and i[step]+a >= 0 and j[step]+b >= 0):
                 
-                x_new_coord.append(x[i[step]][j[step]])
-                y_new_coord.append(y[i[step]][j[step]])
-                z_new_coord.append(z[i[step]][j[step]])
+                if z[i[step]+a][j[step]+b] < z[i[step]][j[step]]:
+                    
+                    x_new.append(x[i[step]+a][j[step]+b])
+                    y_new.append(y[i[step]+a][j[step]+b])
+                    z_new.append(z[i[step]+a][j[step]+b])
+                    i_new.append(i[step]+a)
+                    j_new.append(j[step]+b)
+                    
+                else:
+                    x_new.append(x[i[step]][j[step]])
+                    y_new.append(y[i[step]][j[step]])
+                    z_new.append(z[i[step]][j[step]])
+                    i_new.append(i[step])
+                    j_new.append(j[step])
+                    
+            else:
+                    
+                x_new.append(x[i[step]][j[step]])
+                y_new.append(y[i[step]][j[step]])
+                z_new.append(z[i[step]][j[step]])
                 i_new.append(i[step])
                 j_new.append(j[step])
-                
-        
-        weigth_fish_iter = self.weight(z_mass = [z_new_coord, z], weightScale = weightScale)            
-        fish_coord = np.array([x_new_coord, y_new_coord, z_new_coord])
+                    
+        coord_new = np.array([x_new, y_new, z_new])
         i_j_new = np.array([i_new, j_new])
         
-        return fish_coord, i_j_new, weigth_fish_iter
+                    
+        return coord_new, i_j_new
     
     
-    def step_fish(self, x, y, z, i, j,  x_new, y_new, z_new, i_new, j_new):
+    def delta_f(self, z_coord):
         
-        if z > z_new:
-            return x_new, y_new, z_new, i_new, j_new
+        delta = []
         
+        for i in range(0, len(z_coord[0])):
+            z_ = (z_coord[0][i] - z_coord[1][i])
+            delta.append(z_)
+            
+        return delta
+    
+    def weight(self, z_coord, weightScale):
+        
+        delta = self.delta_f(z_coord = z_coord)
+        
+        delta_max = np.max(delta)
+        
+        if delta_max == 0:
+            delta_max = 1
+        
+        weight_mass = []
+        #print(delta_max)
+        if type(weightScale) == int:
+            
+            for i in range(0, len(z_coord[0])):
+                
+                d = (delta[i]/delta_max)+weightScale
+
+                weight_mass.append(d)
+                
         else:
-            return x, y, z, i, j
+            for i in range(0, len(z_coord[0])):
+                
+                d = (delta[i]/delta_max)+weightScale[i]
+
+                weight_mass.append(d)
+
+        
+        return weight_mass, delta
+        
             
         
+    def instinctive_collective_step(self, coord, i_j, z_coord):
+        
+        delta = self.delta_f(z_coord)
+        d = []
+        
+        delt = np.sum(delta)
+        if delt == 0:
+            delt = 0.1
+        
+        
+        for i in range(len(delta)):
+            
+            d.append(delta[i]**2)
+            
+        m = np.sum(d)/delt
+        m = int(m)
+        #print(m, np.sum(delta))
+        
+        
+        x_new = []
+        y_new = []
+        z_new = []
+        i_new = []
+        j_new = []
+        
+        x, y, z = coord[0], coord[1], coord[2]
+        i, j = i_j[0], i_j[1]
+        
+        for step in range(0,len(i)):
+            
+            if (i[step]+m < len(z) and j[step]+m < len(z) and i[step]+m >= 0 and j[step]+m >= 0):
+                
+                if z[i[step]+m][j[step]+m] < z[i[step]][j[step]]:
+                    
+                    x_new.append(x[i[step]+m][j[step]+m])
+                    y_new.append(y[i[step]+m][j[step]+m])
+                    z_new.append(z[i[step]+m][j[step]+m])
+                    i_new.append(i[step]+m)
+                    j_new.append(j[step]+m)
+                    
+                else:
+                    x_new.append(x[i[step]][j[step]])
+                    y_new.append(y[i[step]][j[step]])
+                    z_new.append(z[i[step]][j[step]])
+                    i_new.append(i[step])
+                    j_new.append(j[step])
+                    
+            else:
+                    
+                x_new.append(x[i[step]][j[step]])
+                y_new.append(y[i[step]][j[step]])
+                z_new.append(z[i[step]][j[step]])
+                i_new.append(i[step])
+                j_new.append(j[step])
+                    
+        coord_new = np.array([x_new, y_new, z_new])
+        i_j_new = np.array([i_new, j_new])
+        
+                    
+        return coord_new, i_j_new
+        
+    def barycenter(self, z_coord, weight):
+        
+        z_wight = []
+        
+        for i in range(0, len(z_coord)):
+            
+            z_wight.append(z_coord)
+        
+        z_sum = np.sum(z_wight)
+        sum_weight = np.sum(weight)
+        
+        bary = z_sum/sum_weight
+        
+        return bary
     
-FSS_alg = FSS()
+    
+    def coll_step(self, coord, i_j, z_coord, individStep, bary_center):
+        
+        coord_new = []
+        
+        for i in range(len(i_j[1])):
+            
+            a = random.randint(0,1)
+            
+            step = (individStep**2)*a*((z_coord[2][i] - bary_center)/(norm(z_coord[2][i] - bary_center)))
+            
+            if math.isnan(step) == True:
+                coord_new.append(0)
+            else:
+                coord_new.append(round(step))
+            
+        
+        
+        #print(coord_new)
+            
+            
+        x_new = []
+        y_new = []
+        z_new = []
+        i_new = []
+        j_new = []
+        
+        x, y, z = coord[0], coord[1], coord[2]
+        i, j = i_j[0], i_j[1]
+        
+        for step in range(0,len(i)):
+            
+            if (i[step]+coord_new[step] < len(z) and j[step]+coord_new[step] < len(z) and i[step]+coord_new[step] >= 0 and j[step]+coord_new[step] >= 0):
+                
+                if z[i[step]+coord_new[step]][j[step]+coord_new[step]] < z[i[step]][j[step]]:
+                    
+                    x_new.append(x[i[step]+coord_new[step]][j[step]+coord_new[step]])
+                    y_new.append(y[i[step]+coord_new[step]][j[step]+coord_new[step]])
+                    z_new.append(z[i[step]+coord_new[step]][j[step]+coord_new[step]])
+                    i_new.append(i[step]+coord_new[step])
+                    j_new.append(j[step]+coord_new[step])
+                    
+                else:
+                    x_new.append(x[i[step]][j[step]])
+                    y_new.append(y[i[step]][j[step]])
+                    z_new.append(z[i[step]][j[step]])
+                    i_new.append(i[step])
+                    j_new.append(j[step])
+                    
+            else:
+                    
+                x_new.append(x[i[step]][j[step]])
+                y_new.append(y[i[step]][j[step]])
+                z_new.append(z[i[step]][j[step]])
+                i_new.append(i[step])
+                j_new.append(j[step])
+                    
+        coord_new = np.array([x_new, y_new, z_new])
+        i_j_new = np.array([i_new, j_new])
+        
+                    
+        return coord_new, i_j_new
+        
+            
+
